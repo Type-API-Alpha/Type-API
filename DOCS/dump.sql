8874abdb9e9810e84cf7a304b063e4dc7edcf9d2
RukimichimoshiP <vitinhorevoredo123@gmail.com>
2024-05-30 16:11:57 -0300

    [FIX] Names and Constraints

--- DOCS/dump.sql
+++ DOCS/dump.sql
@@ -1,17 +1,17 @@
 -- Criação da tabela Equipe sem a constraint de líder
-CREATE TABLE Equipe (
+CREATE TABLE Team (
     id UUID PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL, -- Minimo de 5 caracteres
   	CONSTRAINT name_length CHECK (char_length(name) > 5)
 );
 
 -- Criação da tabela Usuario
-CREATE TABLE Usuario (
+CREATE TABLE User (
     id UUID PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL, -- Minimo de 5 caracteres
     email VARCHAR(255) NOT NULL, -- Regex: nome@email.co
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL, -- Minimo de 8 caracteres com letra e numero
     squad UUID,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE,
@@ -24,6 +24,6 @@ CREATE TABLE Usuario (
 );
 
 -- Adicionando a constraint de líder na tabela Equipe
-ALTER TABLE Equipe
+ALTER TABLE Team
 ADD COLUMN leader UUID,
-ADD CONSTRAINT fk_leader FOREIGN KEY (leader) REFERENCES Usuario(id);
+ADD CONSTRAINT fk_leader FOREIGN KEY (leader) REFERENCES User(id);
